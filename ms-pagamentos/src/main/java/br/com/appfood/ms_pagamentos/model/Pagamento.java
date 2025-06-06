05fd32b43951a2c0b950ba263e70985ef0047a3b
Camilo LR <autumndrums@hotmail.com>
2025-06-06 16:14:26 -0300

    PagamentoService com os métodos Criar, Atualizar, Deletar, ObterTodos, ObterPorId

--- ms-pagamentos/src/main/java/br/com/appfood/ms_pagamentos/model/Pagamento.java
+++ ms-pagamentos/src/main/java/br/com/appfood/ms_pagamentos/model/Pagamento.java
@@ -44,6 +44,7 @@ public class Pagamento {
     @Size(min=3, max=3)
     private String codigo;
 
+    @Setter
     @NotNull
     @Enumerated(EnumType.STRING)
     private Status status;
@@ -53,4 +54,92 @@ public class Pagamento {
 
     @NotNull
     private Long formaDePagamentoId;
+
+    
+    public Pagamento() {
+    }
+
+    public Pagamento(Long id, BigDecimal valor, String nome, String numero, String expiracao, String codigo, Status status, Long pedidoId, Long formaDePagamentoId) {
+        this.id = id;
+        this.valor = valor;
+        this.nome = nome;
+        this.numero = numero;
+        this.expiracao = expiracao;
+        this.codigo = codigo;
+        this.status = status;
+        this.pedidoId = pedidoId;
+        this.formaDePagamentoId = formaDePagamentoId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getExpiracao() {
+        return expiracao;
+    }
+
+    public void setExpiracao(String expiracao) {
+        this.expiracao = expiracao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Long getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(Long pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public Long getFormaDePagamentoId() {
+        return formaDePagamentoId;
+    }
+
+    public void setFormaDePagamentoId(Long formaDePagamentoId) {
+        this.formaDePagamentoId = formaDePagamentoId;
+    }
 }
