2581478110938591f6865211ddfb35e8e89236e9
Camilo LR <autumndrums@hotmail.com>
2025-06-10 16:19:56 -0300

    Microsserviço de Pedidos - Pacote model com as classes Pedido, ItemDoPedido e Enum Status

---
+++ ms-pedidos/src/main/java/br/com/appfodd/ms_pedidos/model/Status.java
@@ -0,0 +1,12 @@
+package br.com.appfodd.ms_pedidos.model;
+
+public enum Status {
+    REALIZADO,
+    CANCELADO,
+    PAGO,
+    NAO_AUTORIZADO,
+    CONFIRMADO,
+    PRONTO,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE;
+}
